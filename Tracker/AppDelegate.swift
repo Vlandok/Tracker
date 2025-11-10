fd15c47e4a90ea19adcc1f5b2830844a9cbde0c6
Vladislav Maskajkin <vmaskajkin@fun.co>
2025-11-10 23:24:12 +0100

    Initial Commit
    Initial commit

---
+++ Tracker/AppDelegate.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: UISceneSession Lifecycle
+    
+    func application(
+       _ application: UIApplication,
+       configurationForConnecting connectingSceneSession: UISceneSession,
+       options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+       let sceneConfiguration = UISceneConfiguration(
+           name: "Main",
+           sessionRole: connectingSceneSession.role
+       )
+       sceneConfiguration.delegateClass = SceneDelegate.self 
+       return sceneConfiguration
+    }
+
+}
+
